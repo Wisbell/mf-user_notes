9e1bc0b4aa1b3e19788ab63134bf85605d8c70d7
Wisbell <westleyisbell@gmail.com>
2017-01-17 10:00:46 -0600

    Created partials.  Connected partials to controllers

--- app.js
+++ app.js
@@ -11,10 +11,48 @@ app.config(function($routeProvider, $locationProvider){
   $locationProvider.hashPrefix('')
 
   $routeProvider
-    .when()
+    .when('/', {
+      controller: 'MainCtrl'
+      templateUrl: ''
+    })
+    .when('/login', {
+      controller: 'LoginCtrl'
+      templateUrl: 'partials/login.html'
+    })
+    .when('/register', {
+      controller: 'RegisterCtrl'
+      templateUrl: 'partials/registration.html'
+    })
+    .when('/note', {
+      controller: 'NoteCtrl'
+      templateUrl: 'partials/new-note.html'
+    })
+    .when('/my-notes', {
+      controller: 'NoteListCtrl'
+      templateUrl: 'partials/note-list.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
+})
+
+app.controller('MainCtrl', function($scope){
+
 })
 
 
-app.controller('MainCtrl', function($scope){
+app.controller('RegisterCtrl', function($scope){
+
+})
+
+app.controller('LoginCtrl', function($scope){
+
+})
+
+app.controller('NoteCtrl', function($scope){
+
+})
+
+app.controller('NoteListCtrl', function($scope){
 
 })
