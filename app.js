c2f897647588a051c47cf6ecd93645cccfa1281e
Wisbell <westleyisbell@gmail.com>
2017-01-17 11:33:49 -0600

    Added bootstrap nav

--- app.js
+++ app.js
@@ -5,54 +5,54 @@ console.log("app.js loaded")
 
 
 // configure angular
-var app = angular.module('userNotesApp', ['ngRoute'])
+var app = angular.module('userNoteApp', ['ngRoute'])
 
 app.config(function($routeProvider, $locationProvider){
   $locationProvider.hashPrefix('')
 
   $routeProvider
-    .when('/', {
-      controller: 'MainCtrl'
-      templateUrl: ''
-    })
-    .when('/login', {
-      controller: 'LoginCtrl'
-      templateUrl: 'partials/login.html'
-    })
-    .when('/register', {
-      controller: 'RegisterCtrl'
-      templateUrl: 'partials/registration.html'
-    })
-    .when('/note', {
+    // .when('/', {
+    //   controller: 'MainCtrl'
+    //   templateUrl: ''
+    // })
+    // .when('/login', {
+    //   controller: 'LoginCtrl'
+    //   templateUrl: 'partials/login.html'
+    // })
+    // .when('/register', {
+    //   controller: 'RegisterCtrl'
+    //   templateUrl: 'partials/registration.html'
+    // })
+    .when('/new-note', {
       controller: 'NoteCtrl'
       templateUrl: 'partials/new-note.html'
     })
-    .when('/my-notes', {
+    .when('/list-notes', {
       controller: 'NoteListCtrl'
       templateUrl: 'partials/note-list.html'
     })
     .otherwise({
-      redirectTo: '/'
+      redirectTo: '/list-notes'
     })
 })
 
-app.controller('MainCtrl', function($scope){
+// app.controller('MainCtrl', function($scope){
 
-})
+// })
 
 
-app.controller('RegisterCtrl', function($scope){
+// app.controller('RegisterCtrl', function($scope){
 
-})
+// })
 
-app.controller('LoginCtrl', function($scope){
+// app.controller('LoginCtrl', function($scope){
 
-})
+// })
 
 app.controller('NoteCtrl', function($scope){
-
+  console.log("Note controller")
 })
 
 app.controller('NoteListCtrl', function($scope){
-
+  console.log("Note List controller")
 })
